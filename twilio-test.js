f0fd0fcaeeaf6f95d70df58b34f1e33c8704d49c
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-04-19 22:12:57 -0500

    Delete twilio-test.js

--- twilio-test.js
+++
@@ -1,9 +0,0 @@
-const client = require('twilio')('ACc45c307e7a541663f867ba9d336e8214', 'd3fd494785656e2233a751fc7fc51595');
-
-client.messages
-  .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+16505675385',
-    to: '+16368914466',
-  })
-  .then((message) => console.log(message.sid));
